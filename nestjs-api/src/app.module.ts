2e1fdee2c9f038b719fd62df9f0464b2395802fe
viviane <vivianeaguiarc@outlook.com>
2024-12-17 20:38:04 -0300

    feat: add -/places capture place_id of a place on google maps

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MapsModule } from './maps/maps.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MapsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
